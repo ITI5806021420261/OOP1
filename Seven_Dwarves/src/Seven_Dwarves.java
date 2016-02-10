5ca47530af1ccb19a3b0246968dec47f2bb95b45
InwYu_Za 007 <InwYu_Za 007@DESKTOP-2CJEG30.mshome.net>
2016-02-11 00:17:30 +0700

    ศรัณยู ก้อนขาว

---
+++ Seven_Dwarves/src/Seven_Dwarves.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+
+public class Seven_Dwarves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Dw1,Dw2,Dw3,Dw4,Dw5,Dw6,Dw7,Dw8,Dw9;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Dw1 : ");
+		Dw1 = scan.nextInt();
+		System.out.print("Input Dw2 : ");
+		Dw2 = scan.nextInt();
+		System.out.print("Input Dw3 : ");
+		Dw3 = scan.nextInt();
+		System.out.print("Input Dw4 : ");
+		Dw4 = scan.nextInt();
+		System.out.print("Input Dw5 : ");
+		Dw5 = scan.nextInt();
+		System.out.print("Input Dw6 : ");
+		Dw6 = scan.nextInt();
+		System.out.print("Input Dw7 : ");
+		Dw7 = scan.nextInt();
+		System.out.print("Input Dw8 : ");
+		Dw8 = scan.nextInt();
+		System.out.print("Input Dw9 : ");
+		Dw9 = scan.nextInt();
+		System.out.println();
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw2");
+		if(Dw2+Dw4+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw3");
+		if(Dw3+Dw2+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw4");
+		if(Dw3+Dw4+Dw2+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw5");
+		if(Dw3+Dw4+Dw5+Dw2+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw6");
+		if(Dw3+Dw4+Dw5+Dw6+Dw2+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw7");
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw1 & Dw8");
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw1 & Dw9");
+		if(Dw1+Dw4+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw3");
+		if(Dw3+Dw1+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw4");
+		if(Dw3+Dw4+Dw1+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw5");
+		if(Dw3+Dw4+Dw5+Dw1+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw6");
+		if(Dw3+Dw4+Dw5+Dw6+Dw1+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw7");
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw1+Dw9==100)
+		System.out.print("Fake Dw is Dw2 & Dw8");
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw8+Dw1==100)
+		System.out.print("Fake Dw is Dw2 & Dw9");
+		if(Dw1+Dw2+Dw5+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw3 & Dw4");
+		if(Dw1+Dw4+Dw2+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw3 & Dw5");
+		if(Dw1+Dw4+Dw5+Dw2+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw3 & Dw6");
+		if(Dw1+Dw4+Dw5+Dw6+Dw2+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw3 & Dw7");
+		if(Dw1+Dw4+Dw5+Dw6+Dw7+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw3 & Dw8");
+		if(Dw1+Dw4+Dw5+Dw6+Dw7+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw3 & Dw9");
+		if(Dw3+Dw1+Dw2+Dw6+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw4 & Dw5");
+		if(Dw3+Dw1+Dw5+Dw2+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw4 & Dw6");
+		if(Dw3+Dw1+Dw5+Dw6+Dw2+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw4 & Dw7");
+		if(Dw3+Dw1+Dw5+Dw6+Dw7+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw4 & Dw8");
+		if(Dw3+Dw1+Dw5+Dw6+Dw7+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw4 & Dw9");
+		if(Dw3+Dw4+Dw1+Dw2+Dw7+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw5 & Dw6");
+		if(Dw3+Dw4+Dw1+Dw6+Dw2+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw5 & Dw7");
+		if(Dw3+Dw4+Dw1+Dw6+Dw7+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw5 & Dw8");
+		if(Dw3+Dw4+Dw1+Dw6+Dw7+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw5 & Dw9");
+		if(Dw3+Dw4+Dw5+Dw1+Dw2+Dw8+Dw9==100)
+		System.out.print("Fake Dw is Dw6 & Dw7");
+		if(Dw3+Dw4+Dw5+Dw1+Dw7+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw6 & Dw8");
+		if(Dw3+Dw4+Dw5+Dw1+Dw7+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw6 & Dw9");
+		if(Dw3+Dw4+Dw5+Dw6+Dw1+Dw2+Dw9==100)
+		System.out.print("Fake Dw is Dw7 & Dw8");
+		if(Dw3+Dw4+Dw5+Dw6+Dw1+Dw8+Dw2==100)
+		System.out.print("Fake Dw is Dw7 & Dw9");
+		if(Dw3+Dw4+Dw5+Dw6+Dw7+Dw1+Dw2==100)
+		System.out.print("Fake Dw is Dw8 & Dw9");
+
+
+	}
+
+}
